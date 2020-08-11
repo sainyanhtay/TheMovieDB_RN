f35f595de6687fdb9220bf314cdfef76e0dd4e75
sainyanhtay <sainyanhtay@gmail.com>
2020-08-11 14:43:39 +0630

    Revert "Revert "UPDATE DETAIL UI by KHL""
    This reverts commit a09478faf5fb7a1dfc0c66b12b8fd238dfbb8d02.

--- src/containers/styles/MovieDetail.js
+++ src/containers/styles/MovieDetail.js
@@ -10,13 +10,31 @@ export default StyleSheet.create({
     color: 'white',
     fontSize: 25,
   },
-  poster: {
+  backgroundView: {
     width: '100%',
     height: '50%',
-    backgroundColor: 'gray',
-    borderBottomRightRadius: 30,
-    borderBottomLeftRadius: 30,
-    marginBottom: 10,
+  },
+  backgroundView2: {
+    width: '100%',
+    height: '20%',    
+  },
+  backgroundView3: {
+    width: '100%',
+    height: '30%',    
+  },
+  poster: {
+    width: '100%',
+    height: '100%',
+    // borderBottomRightRadius: 30,
+    // borderBottomLeftRadius: 30,
+    // marginBottom: 10,
+    flex: 1,
+    resizeMode: 'cover',
+  },
+  poster2: {
+    width: '50%',
+    height: '70%',
+    
   },
   overview: {
     paddingTop: 10,
@@ -30,5 +48,26 @@ export default StyleSheet.create({
   textSection: {
     paddingLeft: 15,
     paddingRight: 15,
+  },
+  movieTitle: {
+    marginTop: 10,
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'white',
+    textAlign:'center',    
+  },
+  movieOverview: {
+    marginTop: 10,
+    fontSize: 15,
+    color: 'white',
+    marginLeft: 10,
+    marginRight: 10  
+  },
+  voteTitle: {
+    marginTop: 10,
+    fontSize: 15,
+    fontWeight: 'bold',
+    color: 'white',
+    textAlign:'center',    
   },
 });
