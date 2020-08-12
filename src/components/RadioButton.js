fb87ee343eff1bb2a7aad6b9572a64eb3e0c8a6d
KaungKhantLin <kaungkhant624@gmail.com>
2020-08-12 13:31:11 +0630

    Student Reg Form

---
+++ src/components/RadioButton.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
+
+export default class RadioButtons extends Component {
+	state = {
+		value: "male",
+	};
+
+	render() {
+		const { options } = this.props;
+		const { value } = this.state;
+
+		return (
+			<View>
+				{options.map(item => {
+					return (
+						<View key={item.key} style={styles.buttonContainer}>
+							<Text style={{ marginTop: 10}}>{item.text}</Text>
+							<TouchableOpacity
+								style={styles.circle}
+								onPress={() => {
+									this.setState({
+										value: item.key,
+									});
+								}}
+							>
+								{value === item.key && <View style={styles.checkedCircle} />}
+							</TouchableOpacity>
+						</View>
+					);
+				})}
+			</View>
+		);
+	}
+}
+
+const styles = StyleSheet.create({
+	buttonContainer: {
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+	},
+
+	circle: {
+        marginLeft: 20,
+        marginTop: 10,
+		height: 20,
+		width: 20,
+		borderRadius: 10,
+		borderWidth: 1,
+		borderColor: '#ACACAC',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+  
+	checkedCircle: {
+		width: 14,
+		height: 14,
+		borderRadius: 7,
+		backgroundColor: '#794F9B',
+	},
+});
--- src/containers/Notification.js
+++ src/containers/Notification.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {View, Text, TextInput, Button} from 'react-native';
+import { Container, Header, Content, Form, Picker, ListItem, Radio, Right, Left } from 'native-base'; 
 import Icon from 'react-native-vector-icons/Ionicons';
 
 class Notification extends Component {
@@ -11,11 +12,59 @@ class Notification extends Component {
       </View>
     ),
   };
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: "key1"
+    };
+  }
+  onValueChange(value) {
+    this.setState({
+      selected: value
+    });
+  }
   render() {
     return (
-      <View>
+      <Container>
         <Text> Notification </Text>
-      </View>
+        <Content>
+          <ListItem selected={false} >
+            <Left>
+              <Text>Lunch Break</Text>
+            </Left>
+            <Right>
+              <Radio
+                selected={false}
+              />
+            </Right>
+          </ListItem>
+          <ListItem selected={true}>
+            <Left>
+              <Text>Discussion with Client</Text>
+            </Left>
+            <Right>
+              <Radio               
+                selected={true}
+              />
+            </Right>
+          </ListItem>
+
+          
+            <Picker
+              note
+              mode="dropdown"
+              style={{ width: 120 }}
+              selectedValue={this.state.selected}
+              onValueChange={this.onValueChange.bind(this)}
+            >
+              <Picker.Item label="Wallet" value="key0" />
+              <Picker.Item label="ATM Card" value="key1" />
+              <Picker.Item label="Debit Card" value="key2" />
+              <Picker.Item label="Credit Card" value="key3" />
+              <Picker.Item label="Net Banking" value="key4" />
+            </Picker>
+        </Content>
+      </Container>
     );
   }
 }
